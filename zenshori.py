913a88441d8177619ed59add40b9dcb57ed14686
Tadele01 <tadeleyednkachw@gmail.com>
2021-05-24 11:59:18 +0300

    link between image file and zenshori file

--- zenshori.py
+++ zenshori.py
@@ -2,7 +2,8 @@ from pathlib import Path
 from distutils.dir_util import copy_tree
 from image import ImageProcessor
 from node import Node
-from typing import  Dict
+from typing import  Dict, List
+from errors import NoPipeline, NoImageDirectory
 
 import os
 import shutil
@@ -10,32 +11,33 @@ import shutil
 
 
 class Zenshori:
-    def __init__(self, folder_name:str, dest=None)->None:
+    def __init__(self, folder_name:str, dest:str=None)->None:
         '''
             Base class 
             
             params:
                 folder_name : A folder path 
-                dest : A folder where the new preprocessed images will be saved 
-                            if not given it takes pythons's cwd
+                dest : A folder where the new processed images will be saved 
+                            if not given it takes cwd
 
             
         '''
         self.folder:str = folder_name
-        self.new_folder:str = 'preprocessed_' + self.folder
+        self.images_folder:str = 'preprocessed_' + self.folder
         self.image_processor:object = None
-        self.graph = None
+        self.graph:List[Node] = None
+        self.images = None
 
         if dest:
-            self.new_folder:str = dest + '/' + self.new_folder
+            self.images_folder = dest + '/' + self.images_folder
         
         try:
-            copy_tree(self.folder, self.new_folder)
+            copy_tree(self.folder, self.images_folder)
+            self.images = ImageProcessor(self.images_folder).load_images()
         except Exception as exp:
             print(exp)     
             return None
-
-        # self.image_processor = ImageProcessor(self.new_folder)
+        
 
     def pipeline(self, functions:Dict)->None:
         cur_node:Node = self.graph
@@ -51,18 +53,27 @@ class Zenshori:
             cur_node.next = node
             cur_node = node
 
-
-        
-    
+    def processs_images(self)-> str:
+        if self.images and self.graph:
+            ImageProcessor.processor(self.images, self.graph)
+            return self.images_folder
+        else:
+            if not self.images:
+                print('No images directory provided')
+                raise NoImageDirectory
+            if not self.images:
+                print('No processing pipline provided')
+                raise NoPipeline
+            
     def inspect(self):
         print("Preprocessing Steps")
-        separator = "====================================================================="
-        table = '---------------------------------------------------------------------'
+        separator:str = "====================================================================="
+        table:str = '---------------------------------------------------------------------'
         print(table)
         print('{0: <40}'.format("Processing functions"), "Parameters", sep='')
         print(table)
-        head = self.graph
-        while head != None:
+        head:Node = self.graph
+        while head:
             print('{0: <40}'.format(head.function_name), end='')
             print(head.params, sep='\n')
             head = head.next
@@ -79,7 +90,6 @@ import math
 zen.pipeline({abs:[1, 2, 3] , math.ceil:[12, 13, 14]})
 
 zen.inspect()
-
 
 
 
