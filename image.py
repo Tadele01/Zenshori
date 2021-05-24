fa263209f1d39f4196a2f981c99e4723664b0ea7
Tadele01 <tadeleyednkachw@gmail.com>
2021-05-24 11:58:51 +0300

    initial processor

--- image.py
+++ image.py
@@ -1,11 +1,16 @@
+from node import Node
 from pathlib import Path
+from typing import List
+from node import Node
+from PIL import Image
+
 import cv2
 import numpy as np
+
 
 class ImageProcessor:
     def __init__(self, folder:str)->None:
         self.folder:str = folder
-        self.images:'generator' = self.load_images()
 
     def load_images(self)->'generator':
         data_dir:str = Path(self.folder)
@@ -13,16 +18,20 @@ class ImageProcessor:
 
         return all_images
 
-    
+    def processor(self, images:'generator', graph:List[Node]):
+        root_node:Node = graph
+        for img_path in images:
+            img = Image.open(img_path)
+            img_matrix = np.asarray(img)
+            while graph:
+                graph.function(img_matrix, args=graph.args)
+                img_matrix = img_matrix.astype(np.uint8)
+                img = Image.fromarray(img)
+                save_path = str(img_path)
+                img.save(save_path)
+            
+            graph = root_node
+        
 
-    
-
-    
-
-    
-
-img = ImageProcessor('Fundus_Train_Val_Data')
-
-print(type(img.load_images()))
 
 
